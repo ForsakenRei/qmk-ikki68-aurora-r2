12a0ede358210661b264e9d3dd967b47cddf852b
ForsakenRei <23041178+ForsakenRei@users.noreply.github.com>
2022-11-13 13:50:15 -0500

    misc update

--- wuque/ikki68_aurora/keymaps/shigure/keymap.c
+++ wuque/ikki68_aurora/keymaps/shigure/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 shigure
+/* Copyright 2022 Shigure
  * 
  * This program is free software: you can redistribute it and/or modify 
  * it under the terms of the GNU General Public License as published by 
